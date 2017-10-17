c3b5ae70ea0f5bd44164cb152d28208f5550d99e
blueslee <bluezhin@gmail.com>
2017-10-17 15:43:26 +0800

    first commit

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    private float count = 3;
+    private float speed = 100;
+    private float wrapPoint = 400;
+    public float position;
+
+    void Start()
+    {
+        position = Vector3.Dot(transform.position, transform.forward);
+    }
+
+    void Update()
+    {
+        position += Time.deltaTime * speed;
+
+        if (position > wrapPoint) position -= wrapPoint * count;
+
+        transform.position = transform.forward * position;
+    }
+}
---
+++ Assets/Scripts/TitleControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleControl : MonoBehaviour
+{
+    public Button NormalButton;
+    public Button VRButton;
+
+    // Use this for initialization
+    void Start ()
+    {
+        NormalButton.onClick.AddListener(OnNormalButtonClick);
+        VRButton.onClick.AddListener(OnVRButtonClick);
+    }
+	
+	private void OnNormalButtonClick()
+    {
+        // ....
+        SceneManager.LoadScene("MainScene");
+    }
+
+    private void OnVRButtonClick()
+    {
+        // ....
+        SceneManager.LoadScene("MainScene");
+    }
+}
